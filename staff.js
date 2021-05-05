0e65db89b8125cf4fbac18f1ef12e308fa067976
Alf <57676676+alfredsaveron@users.noreply.github.com>
2021-05-05 15:05:52 +0300

    Create staff.js

---
+++ staff.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items:4,
+      autoplay:false,
+      margin:25,
+      loop:true,
+      nav:true,
+      navText:["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>" ]
+  });
+});
